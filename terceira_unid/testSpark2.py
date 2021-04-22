e41f5e1f7b84a97ae6b48955f276c84dc0406f7a
Marcos Delgado <marcosdelgado0408@gmail.com>
2021-04-22 09:54:00 -0300

    Adicionando restante do algoritmo -> funcionando

--- terceira_unid/testSpark2.py
+++ terceira_unid/testSpark2.py
@@ -1,10 +1,10 @@
 import findspark
-
-findspark.init()
-
+import math
 from pyspark import SparkContext
 from pyspark.streaming import StreamingContext
 from pyspark.streaming.kafka import KafkaUtils, OffsetRange
+findspark.init()
+
 
 
 class Point(object):
@@ -13,6 +13,15 @@ class Point(object):
         self.y = y
         self.value = value
         self.distance = 0
+
+
+
+
+
+# Testing point
+testPoint = Point(2, 7.45, 0)
+k = 3
+
 
 
 
@@ -27,7 +36,6 @@ def printPoints(x):
     print("x-> {}, y->{}, val->{}, dist -> {}".format(x.x, x.y, x.value, x.distance))
 
 
-
 # 3 -> x / 4 -> y / 12 -> val
 def criarPoints(x):
     splittedLinha = x.split(",")
@@ -37,10 +45,25 @@ def criarPoints(x):
     return eachPoint
 
 
+def setDistance(eachpoint):
+    # testPoint = global testPoint
+
+    eachpoint.distance = math.sqrt( (float(eachpoint.x) - testPoint.x) * (float(eachpoint.x) - testPoint.x) + (float(eachpoint.y) - testPoint.y) * (float(eachpoint.y) - testPoint.y) )
+
+    return eachpoint
+
+
+def pegarVal(x):
+    return x.value
 
 
 
-
+def media(lista):
+    media = 0
+    for i in lista:
+        media += float(i)
+    
+    return media/len(lista)
 
 
 sc = SparkContext(appName="Knn")
@@ -48,16 +71,13 @@ sc = SparkContext(appName="Knn")
 kafkaParams = {"metadata.broker.list": "localhost:9092"}
 
 start = 1 # pular primeira linha
-until = 5
+until = 15
 partition = 0
 topic = 'csvtopic'
 offset = OffsetRange(topic, partition, start, until)
 offsets = [offset]
 
 
-# Testing point
-testPoint = Point(2, 7.45, 0)
-k = 3
 
 
 
@@ -68,20 +88,35 @@ rdd = KafkaUtils.createRDD(sc, kafkaParams, offsets)
 
 linhas = rdd.map(lambda x: x[1])
 
-
 linhas.foreach(printer)
 
-arr = linhas.map(criarPoints).foreach(printPoints)
+
+
+arr = linhas.map(criarPoints) # Criacao dos points 
+
+
+arr.foreach(printPoints)
+print("\n")
+
+dist = arr.map(setDistance) # Distancia euclidiana
+
+dist.foreach(printPoints)
+print("\n")
+
+orderedDist = dist.sortBy(lambda x: x.distance) # Ordenacao
+
+orderedDist.foreach(printPoints)
 
 
 
+valores = orderedDist.map(pegarVal)
+
+# pegando os k primeiros valores -> ja no formato de lista
+lista = valores.take(k)
+print(lista)
+print("The value classified to unknown point is: {}".format(media(lista)))
 
 
-
-
-
-
-# lista = rdd.collect()
 
 
 
